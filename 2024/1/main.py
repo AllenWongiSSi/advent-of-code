cc9468b37cceb7b7edd136fad87823f4cdda1b5a
Allen W <allen.wong@iss-i.ca>
2024-12-02 07:00:08 -0800

    day 1 part 2 in Python

--- 2024/1/main.py
+++ 2024/1/main.py
@@ -7,12 +7,16 @@ def main():
     
     data = [line_data.split() for line_data in data]
     list1, list2 = zip(*data)
-    
+
     list1 = sorted(list1)
     list2 = sorted(list2)
 
     differences = [abs(int(a) - int(b)) for a,b in zip(list1, list2)]
-    print(sum(differences))
+    " day 1 part 1 answer "
+    print(f"Total Distance = {sum(differences)}")
+    " day 1 part 2 answer "
+    similarity_score_list = [list2.count(l1_element) * int(l1_element) for l1_element in list1]
+    print(f"Sum of Similarity Score = {sum(similarity_score_list)}")
 
 if __name__ == "__main__":
     main()
